0c9bccd3d8b5906011568a3511a71f484e46d36f
ridwan257 <46244981+ridwan257@users.noreply.github.com>
2024-02-26 00:02:19 +0600

    Update run.R

--- apiR/run.R
+++ apiR/run.R
@@ -1,4 +1,25 @@
-library(plumber)
+install.packages("base64enc")
+install.packages("jsonlite")
+install.packages("png")
+install.packages("pheatmap")
+install.packages("RColorBrewer")
+install.packages("devtools")
+install.packages("rafalib")
+install.packages("plumber")
+install.packages("BiocManager")
+install.packages('ggplot2')
+install.packages('dplyr')
+install.packages('ggrepel')
+install.packages('limma')
 
+
+library(BiocManager)
+
+BiocManager::install("BiocGenerics")
+BiocManager::install("GEOquery")
+BiocManager::install("Biobase")
+
+
+library(plumber)
 
 plumb('./plumber.R')$run(port=8000)
